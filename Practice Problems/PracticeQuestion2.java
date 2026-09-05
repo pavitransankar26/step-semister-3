bebb4b10d4c1b31677377284b89d606e5099a40f
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 19:48:46 +0530

    Payroll Batch Bonus Round

--- Practice Problems/PracticeQuestion2.java
+++ Practice Problems/PracticeQuestion2.java
@@ -1,1 +1,36 @@
+class Employee {
+    String id;
+    double salary;
 
+    Employee(String id, double salary) {
+        this.id = id;
+        this.salary = salary;
+    }
+
+    void raiseSalary(double salary) {
+        this.salary = this.salary + salary;
+    }
+
+    void printSalary() {
+        System.out.println(id + " | Final Salary: Rs " + salary);
+    }
+}
+
+public class PracticeQuestion2 {
+    public static void main(String[] args) {
+
+        double[] salaries = {40000, 55000, 62000, 48000};
+
+        Employee[] employees = {
+            new Employee("E-101", salaries[0]),
+            new Employee("E-102", salaries[1]),
+            new Employee("E-103", salaries[2]),
+            new Employee("E-104", salaries[3])
+        };
+
+        for (Employee employee : employees) {
+            employee.raiseSalary(5000);
+            employee.printSalary();
+        }
+    }
+}
