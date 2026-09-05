1b56b9874053f0df86faa5589cc89b66b154e2f9
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 19:54:44 +0530

    Static Block – Library Membership Card Setup

--- Assignments/AssignmentQuestion4.java
+++ Assignments/AssignmentQuestion4.java
@@ -1,1 +1,45 @@
+class MembershipCard {
 
+    static String libraryName;
+    static String validUntil;
+
+    String studentName;
+
+    static {
+        libraryName = "SRM Central Library";
+        validUntil = "May 2027";
+
+        System.out.println("Library info loaded");
+    }
+
+    MembershipCard(String studentName) {
+        this.studentName = studentName;
+    }
+
+    void printCard() {
+        System.out.println(
+            "Membership card issued: " + studentName
+        );
+    }
+}
+
+public class AssignmentQuestion4 {
+    public static void main(String[] args) {
+
+        String[] names = {
+            "Ananya",
+            "Rohan",
+            "Priya",
+            "Arjun",
+            "Sneha"
+        };
+
+        for (int i = 0; i < names.length; i++) {
+
+            MembershipCard card =
+                new MembershipCard(names[i]);
+
+            card.printCard();
+        }
+    }
+}
