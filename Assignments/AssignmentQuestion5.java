e32afb355a300094e6e503f8f5c758794b474c35
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 19:36:50 +0530

    Instance vs Static Employee Class

--- Assignments/AssignmentQuestion5.java
+++ Assignments/AssignmentQuestion5.java
@@ -1,1 +1,38 @@
+class EmployeeDetails {
+    String empName;
+    double salary;
 
+    static String companyName =
+            "Bright Horizon Technologies";
+
+    static int employeeCount = 0;
+
+    EmployeeDetails(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: "
+                + employeeCount);
+    }
+}
+
+public class AssignmentQuestion5 {
+    public static void main(String[] args) {
+
+        EmployeeDetails emp1 =
+                new EmployeeDetails("Arun", 50000);
+
+        EmployeeDetails emp2 =
+                new EmployeeDetails("Divya", 60000);
+
+        EmployeeDetails emp3 =
+                new EmployeeDetails("Kumar", 55000);
+
+        EmployeeDetails.printCompanyInfo();
+    }
+}
