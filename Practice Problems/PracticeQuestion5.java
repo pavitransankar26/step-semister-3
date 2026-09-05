8ca1f9d4882a8d28c9fc5cd5713537fe3a4b9af6
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 20:01:04 +0530

    Immutable Booking Receipt & Nightly Settlement

--- Practice Problems/PracticeQuestion5.java
+++ Practice Problems/PracticeQuestion5.java
@@ -1,1 +1,50 @@
+final class BookingReceipt {
 
+    private final String bookingId;
+    private final String[] seatNumbers;
+
+    public BookingReceipt(
+            String bookingId,
+            String[] seatNumbers) {
+
+        this.bookingId = bookingId;
+
+        this.seatNumbers =
+            new String[seatNumbers.length];
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+            this.seatNumbers[i] = seatNumbers[i];
+        }
+    }
+
+    public String[] getSeatNumbers() {
+
+        String[] copy =
+            new String[seatNumbers.length];
+
+        for (int i = 0; i < seatNumbers.length; i++) {
+            copy[i] = seatNumbers[i];
+        }
+
+        return copy;
+    }
+
+    public BookingReceipt withUpdatedSeat(
+            int index,
+            String newSeat) {
+
+        String[] updatedSeats =
+            getSeatNumbers();
+
+        if (index >= 0 &&
+            index < updatedSeats.length) {
+
+            updatedSeats[index] = newSeat;
+        }
+
+        return new BookingReceipt(
+            bookingId,
+            updatedSeats
+        );
+    }
+}
