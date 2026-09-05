89fad3aa6d9331653922f1f9dadbba8bc3b51e31
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 19:49:57 +0530

    One-Time College Setup

--- Practice Problems/PracticeQuestion4.java
+++ Practice Problems/PracticeQuestion4.java
@@ -1,1 +1,40 @@
+class SrmStudent {
 
+    String name;
+
+    static String collegeName;
+    static String academicYear;
+
+    static {
+        collegeName = "SRM Institute of Science and Technology";
+        academicYear = "2026-2027";
+
+        System.out.println("College info loaded");
+    }
+
+    SrmStudent(String name) {
+        this.name = name;
+    }
+
+    void printConfirmation() {
+        System.out.println("Student record created: " + name);
+    }
+}
+
+public class PracticeQuestion4 {
+    public static void main(String[] args) {
+
+        String[] names = {
+            "Ravi",
+            "Meera",
+            "Karthik",
+            "Divya",
+            "Anitha"
+        };
+
+        for (String name : names) {
+            SrmStudent student = new SrmStudent(name);
+            student.printConfirmation();
+        }
+    }
+}
