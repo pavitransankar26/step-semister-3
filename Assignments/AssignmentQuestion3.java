104a0584e92c3208adbdbf48ab69d6f506c9f423
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 20:09:06 +0530

    Book Copy Circulation Guard

--- Assignments/AssignmentQuestion3.java
+++ Assignments/AssignmentQuestion3.java
@@ -1,1 +1,57 @@
+class BookInventory {
 
+    private int copiesTotal;
+    private int copiesAvailable;
+
+    BookInventory(int copiesTotal) {
+
+        this.copiesTotal = copiesTotal;
+        this.copiesAvailable = copiesTotal;
+    }
+
+    void checkOut() {
+
+        if (copiesAvailable > 0) {
+            copiesAvailable--;
+        }
+    }
+
+    void checkIn() {
+
+        if (copiesAvailable < copiesTotal) {
+            copiesAvailable++;
+        }
+    }
+
+    int getCopiesAvailable() {
+        return copiesAvailable;
+    }
+}
+
+public class AssignmentQuestion3 {
+
+    public static void main(String[] args) {
+
+        BookInventory b = new BookInventory(3);
+
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+        b.checkOut();
+
+        System.out.println(
+                "Available copies: " +
+                        b.getCopiesAvailable()
+        );
+
+        b.checkIn();
+        b.checkIn();
+        b.checkIn();
+        b.checkIn();
+
+        System.out.println(
+                "Available copies: " +
+                        b.getCopiesAvailable()
+        );
+    }
+}
