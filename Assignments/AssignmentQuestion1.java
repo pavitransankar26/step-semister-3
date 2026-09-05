f92e9f2f132426af2fbd04c2c88cf6d377344769
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 20:07:49 +0530

    Membership Field Reach Checker

--- Assignments/AssignmentQuestion1.java
+++ Assignments/AssignmentQuestion1.java
@@ -1,1 +1,103 @@
+class LibraryMember {
+    private String membershipPin;
+    String branchCode;
+    protected double finesOwed;
+    public String displayName;
+}
 
+class AccessChecker {
+
+    static String classifyAccess(String fieldModifier, String accessorContext) {
+
+        if (fieldModifier.equals("private")) {
+            if (accessorContext.equals("SAME_CLASS")) {
+                return "ALLOWED";
+            } else {
+                return "DENIED";
+            }
+        }
+
+        if (fieldModifier.equals("default")) {
+            if (accessorContext.equals("SAME_CLASS") ||
+                    accessorContext.equals("SAME_PACKAGE")) {
+                return "ALLOWED";
+            } else {
+                return "DENIED";
+            }
+        }
+
+        if (fieldModifier.equals("protected")) {
+            if (accessorContext.equals("SAME_CLASS") ||
+                    accessorContext.equals("SAME_PACKAGE")) {
+                return "ALLOWED";
+            } else {
+                return "DENIED";
+            }
+        }
+
+        if (fieldModifier.equals("public")) {
+            return "ALLOWED";
+        }
+
+        return "DENIED";
+    }
+
+    static String summarizeByModifier(String[][] attempts) {
+
+        String[] modifiers = {
+                "private", "default", "protected", "public"
+        };
+
+        String result = "";
+
+        for (int i = 0; i < modifiers.length; i++) {
+
+            int allowed = 0;
+            int denied = 0;
+
+            for (int j = 0; j < attempts.length; j++) {
+
+                if (attempts[j][0].equals(modifiers[i])) {
+
+                    String access =
+                            classifyAccess(attempts[j][0], attempts[j][1]);
+
+                    if (access.equals("ALLOWED")) {
+                        allowed++;
+                    } else {
+                        denied++;
+                    }
+                }
+            }
+
+            result += modifiers[i] + ": " + allowed +
+                    " allowed / " + denied + " denied";
+
+            if (i < modifiers.length - 1) {
+                result += " | ";
+            }
+        }
+
+        return result;
+    }
+}
+
+public class AssignmentQuestion1 {
+
+    public static void main(String[] args) {
+
+        String[][] attempts = {
+                {"private", "SAME_CLASS"},
+                {"private", "SAME_PACKAGE"},
+                {"default", "SAME_PACKAGE"},
+                {"default", "DIFFERENT_PACKAGE"},
+                {"protected", "SAME_PACKAGE"},
+                {"protected", "SAME_CLASS"},
+                {"public", "DIFFERENT_PACKAGE"}
+        };
+
+        System.out.println(
+                AccessChecker.summarizeByModifier(attempts)
+        );
+    }
+}
