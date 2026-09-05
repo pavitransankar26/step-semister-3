25ebbdb45720ee1be90908950824b0abd39f5430
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-05 19:59:56 +0530

    Seat Booking Encapsulation Guard

--- Practice Problems/PracticeQuestion3.java
+++ Practice Problems/PracticeQuestion3.java
@@ -1,1 +1,55 @@
+class CineScreen {
 
+    private int seatsTotal;
+    private int seatsAvailable;
+
+    public CineScreen(int seatsTotal) {
+
+        if (seatsTotal <= 0) {
+            throw new IllegalArgumentException(
+                "Seats total must be greater than zero"
+            );
+        }
+
+        this.seatsTotal = seatsTotal;
+        this.seatsAvailable = seatsTotal;
+    }
+
+    public void bookSeat() {
+
+        if (seatsAvailable > 0) {
+            seatsAvailable--;
+        }
+    }
+
+    public void cancelBooking() {
+
+        if (seatsAvailable < seatsTotal) {
+            seatsAvailable++;
+        }
+    }
+
+    public int getSeatsAvailable() {
+        return seatsAvailable;
+    }
+}
+
+public class PracticeQuestion3 {
+
+    public static void main(String[] args) {
+
+        CineScreen c = new CineScreen(2);
+
+        c.bookSeat();
+        c.bookSeat();
+        c.bookSeat();
+
+        System.out.println(c.getSeatsAvailable());
+
+        c.cancelBooking();
+        c.cancelBooking();
+        c.cancelBooking();
+
+        System.out.println(c.getSeatsAvailable());
+    }
+}
