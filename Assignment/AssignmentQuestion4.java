4d09cae665d175259c0352789987c8375539a3ad
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-19 10:49:39 +0530

    The Monthly Attendance Announcer

--- Assignment/AssignmentQuestion4.java
+++ Assignment/AssignmentQuestion4.java
@@ -1,1 +1,75 @@
+class GymMember {
+    String memberId;
+    int monthlyFee;
+    int sessionsAttended;
 
+    public GymMember(String memberId, int monthlyFee) {
+        this.memberId = memberId;
+        this.monthlyFee = monthlyFee;
+        this.sessionsAttended = 0;
+    }
+
+    void attendSession() {
+        sessionsAttended++;
+    }
+
+    int getSessionsAttended() {
+        return sessionsAttended;
+    }
+
+    String displayInfo() {
+        return "Standard | Sessions: " + sessionsAttended;
+    }
+}
+
+class PremiumMember extends GymMember {
+    String trainerName;
+
+    public PremiumMember(String memberId, int monthlyFee,
+                         String trainerName) {
+        super(memberId, monthlyFee);
+        this.trainerName = trainerName;
+    }
+
+    @Override
+    String displayInfo() {
+        return "Premium | Trainer: " + trainerName
+                + " | Sessions: " + sessionsAttended;
+    }
+}
+
+public class AssignmentQuestion4 {
+
+    static String batchPrint(GymMember[] members) {
+
+        StringBuilder result = new StringBuilder();
+
+        for (GymMember member : members) {
+
+            result.append(member.displayInfo());
+
+            if (member instanceof PremiumMember) {
+                PremiumMember premium =
+                    (PremiumMember) member;
+
+                result.append(" [Trainer via downcast: ")
+                      .append(premium.trainerName)
+                      .append("]");
+            }
+
+            result.append(" | ");
+        }
+
+        return result.toString();
+    }
+
+    public static void main(String[] args) {
+
+        GymMember[] members = {
+            new GymMember("MEM6", 1000),
+            new PremiumMember("MEM7", 2000, "Coach Riya")
+        };
+
+        System.out.println(batchPrint(members));
+    }
+}
