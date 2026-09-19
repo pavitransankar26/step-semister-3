d5568927bab46f00f4aa140b7d09d1c02e9cc5c3
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-19 11:03:40 +0530

    Orchestra Warm-Up Routine

--- Practice Problems/PracticeQuestion3.java
+++ Practice Problems/PracticeQuestion3.java
@@ -1,1 +1,33 @@
+abstract class Instrument {
 
+    public abstract String play();
+}
+
+class StringInstrument extends Instrument {
+
+    @Override
+    public String play() {
+        return "Strumming the strings";
+    }
+}
+
+class Violin extends StringInstrument {
+
+    @Override
+    public String play() {
+        return super.play() +
+               ", with a bow drawn across four strings";
+    }
+}
+
+public class PracticeQuestion3 {
+
+    public static void main(String[] args) {
+
+        StringInstrument s = new StringInstrument();
+        Violin v = new Violin();
+
+        System.out.println(s.play());
+        System.out.println(v.play());
+    }
+}
