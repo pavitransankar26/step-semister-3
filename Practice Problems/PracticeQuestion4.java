419d9a09c708b0207b63633b94a403cc60b7ad5a
pavitransankar26 <pavitransankar26@gmail.com>
2026-09-19 11:04:12 +0530

    Smart Kitchen Assistant

--- Practice Problems/PracticeQuestion4.java
+++ Practice Problems/PracticeQuestion4.java
@@ -1,1 +1,55 @@
+abstract class KitchenTool {
 
+    private int speedLevel;
+
+    public abstract String prepare();
+
+    int getSpeedLevel() {
+        return speedLevel;
+    }
+
+    void setSpeedLevel(int speedLevel) {
+        if (speedLevel >= 1 && speedLevel <= 5) {
+            this.speedLevel = speedLevel;
+        }
+    }
+}
+
+interface Washable {
+    String clean();
+}
+
+class Blender extends KitchenTool implements Washable {
+
+    public Blender() {
+    }
+
+    @Override
+    public String prepare() {
+        return "Blending at speed " + getSpeedLevel();
+    }
+
+    @Override
+    public String clean() {
+        return "Blender rinsed and dried";
+    }
+}
+
+public class PracticeQuestion4 {
+
+    public static void main(String[] args) {
+
+        Blender b = new Blender();
+
+        b.setSpeedLevel(3);
+
+        System.out.println(b.getSpeedLevel());
+
+        b.setSpeedLevel(9);
+
+        System.out.println(b.getSpeedLevel());
+
+        System.out.println(b.prepare());
+        System.out.println(b.clean());
+    }
+}
